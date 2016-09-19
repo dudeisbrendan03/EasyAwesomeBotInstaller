2a10438bd8c0e5523dfe628b99b73cb5470bcc6a
dudeisbrendan03 <jbrendan70@gmx.com>
2016-09-19 22:40:54 +0100

    Installs node 5.1.1

--- installawesomebot.sh
+++ installawesomebot.sh
@@ -1,5 +1,8 @@
 
 echo 'Please wait'
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n 5.1.1
 git clone https://github.com/BitQuote/AwesomeBot.git
 
 echo -ne '#####                     (33%)\r'
@@ -13,3 +16,5 @@ cd AwesomeBot
 npm install discord.js@5.0.0
 echo 'Install complete, run type: node bot.js into your console in the new directory AwesomeBot'
 echo 'For web server configuration modify data/config.json. If you need any assistance join the discord.'
+echo 'The script will now launch the auth file. For all the API keys to be entered in.'
+nano ~/AwesomeBot/auth.json
